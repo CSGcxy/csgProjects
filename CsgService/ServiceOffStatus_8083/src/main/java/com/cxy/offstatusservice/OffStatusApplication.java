b73eaecfccb956918d4779e525edc23b61267e13
Your Name <you@example.com>
2021-12-09 17:21:33 +0800

    完善模块

---
+++ CsgService/ServiceOffStatus_8083/src/main/java/com/cxy/offstatusservice/OffStatusApplication.java
@@ -0,0 +1,15 @@
+package com.cxy.offstatusservice;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@MapperScan("com.cxy.netsegservice.mapper")
+@ComponentScan(basePackages = {"com.cxy"}) //添加扫描路径，扫描上一层路径（也包括跨模块组件)
+public class OffStatusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OffStatusApplication.class, args);
+    }
+}
