6eb5c884dd2347872504ae632b7c1ee4166809e0
bynn <bynn>
2022-03-25 01:22:43 +0800

    修改了swagger版本

--- Common/service_base/src/main/java/com/cxy/baseService/config/SwaggerConfig.java
+++ Common/service_base/src/main/java/com/cxy/baseService/config/SwaggerConfig.java
@@ -1,37 +1,35 @@
 package com.cxy.baseService.config;
 
-import com.google.common.base.Predicates;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
+@EnableOpenApi
 public class SwaggerConfig {
     @Bean
-    public Docket webApiConfig(){
-
-        return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("webApi")
-                .apiInfo(webApiInfo())
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
                 .select()
-                .paths(Predicates.not(PathSelectors.regex("/admin/.*")))
-                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+//                .paths(PathSelectors.regex("/swagger/.*"))
                 .build();
     }
 
-    private ApiInfo webApiInfo(){
+    private ApiInfo apiInfo(){
 
         return new ApiInfoBuilder()
                 .title("CSG监控系统API文档")
                 .description("本文档描述了CSG监控系统微服务接口定义")
-                .version("1.0")
+                .version("2.0")
                 .contact(new Contact("Cxy", "http://www.cxy.com", "dajiage@sb.com"))
                 .build();
     }
--- README.md
+++ README.md
@@ -3,10 +3,10 @@ xxx监控系统的后端(基于SpringCloud)
 
 # 有四个服务模块，分别运行在 8081、8082、8083、8084端口，通过以下链接访问对应的swagger页面
 
-http://localhost:8081/terminalStatus/swagger-ui.html
+http://localhost:8081/terminalStatus/swagger-ui/index.html
 
-http://localhost:8082/flows/swagger-ui.html
+http://localhost:8082/flows/swagger-ui/index.html
 
-http://localhost:8083/offTerminalStatus/swagger-ui.html
+http://localhost:8083/offTerminalStatus/swagger-ui/index.html
 
-http://localhost:8084/checkFormat/swagger-ui.html
+http://localhost:8084/checkFormat/swagger-ui/index.html
